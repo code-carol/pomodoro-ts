be8a8e7a13d6277d6903e7115d838c83708d7a58
code-carol <cveloso0808@gmail.com>
2024-03-15 21:42:48 +0000

    Create html and css for the pomodoro app

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,61 @@
+import { useState } from "react";
 import Button from "../../components/Button";
 import Footer from "../../components/Footer";
 import "./Home.css";
 
 const Home = () => {
+  const [isStarted, setIsStarted] = useState(false);
+
   const handleOnStart = () => {};
   const handleOnPause = () => {};
   const handleOnReset = () => {};
   return (
     <>
-      <h1>Pomodoro 🍅</h1>
-      <p>25:00</p>
-      <Button className="start-btn" type="button" handleOnClick={handleOnStart}>
-        Start
-      </Button>
-      <Button className="pause-btn" type="button" handleOnClick={handleOnPause}>
-        Pause
-      </Button>
-      <Button className="reset-btn" type="button" handleOnClick={handleOnReset}>
-        Reset
-      </Button>
+      <div className="container">
+        <div className="center-container">
+          <h1 className="title">Pomodoro 🍅</h1>
+
+          <div className="timer">
+            <div className="progress-bar">
+              <div className="inner-bar">
+                <h2>25:00</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className="buttons">
+            {isStarted ? (
+              <>
+                <Button
+                  className="btn"
+                  id="pause-btn"
+                  type="button"
+                  handleOnClick={handleOnPause}
+                >
+                  Pause
+                </Button>
+                <Button
+                  className="btn"
+                  id="reset-btn"
+                  type="button"
+                  handleOnClick={handleOnReset}
+                >
+                  Reset
+                </Button>
+              </>
+            ) : (
+              <Button
+                className="btn"
+                id="start-btn"
+                type="button"
+                handleOnClick={handleOnStart}
+              >
+                Start
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
