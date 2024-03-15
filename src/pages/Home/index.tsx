b17b6140a3b4c619407f6e8dbf5d4c657414055a
code-carol <cveloso0808@gmail.com>
2024-03-15 20:22:11 +0000

    Create html structure for home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,25 @@
+import Button from "../../components/Button";
+import Footer from "../../components/Footer";
 import "./Home.css";
 
 const Home = () => {
+  const handleOnStart = () => {};
+  const handleOnPause = () => {};
+  const handleOnReset = () => {};
   return (
     <>
-      <h1>Home</h1>
+      <h1>Pomodoro 🍅</h1>
+      <p>25:00</p>
+      <Button className="start-btn" type="button" handleOnClick={handleOnStart}>
+        Start
+      </Button>
+      <Button className="pause-btn" type="button" handleOnClick={handleOnPause}>
+        Pause
+      </Button>
+      <Button className="reset-btn" type="button" handleOnClick={handleOnReset}>
+        Reset
+      </Button>
+      <Footer />
     </>
   );
 };
