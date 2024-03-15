f1be10ee19650e19ad7f9b98cba3def9f38e48de
code-carol <cveloso0808@gmail.com>
2024-03-15 20:22:32 +0000

    Create types file

---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type FormEvent = React.FormEvent<HTMLFormElement>;
+export type MouseEvent = React.MouseEvent<HTMLButtonElement>;
+export type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
